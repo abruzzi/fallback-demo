f8a7ceaccec27b2770ad7ade07eca2168d33343c
Qiu Juntao <juntao.qiu@gmail.com>
2016-08-29 22:00:39 +1000

    add hystrix fallback

--- src/main/java/com/thoughtworks/fallback/Main.java
+++ src/main/java/com/thoughtworks/fallback/Main.java
@@ -4,6 +4,8 @@ import com.thoughtworks.fallback.client.PersonServiceClient;
 import com.thoughtworks.fallback.command.PersonCommand;
 import com.thoughtworks.fallback.domain.Person;
 import feign.Feign;
+import feign.Param;
+import feign.hystrix.HystrixFeign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 
@@ -11,16 +13,47 @@ public class Main {
 
 
     public static void main(String[] args) {
+        basicHystrixCommand();
+        feignWithHystrixFallback();
+        feign();
+    }
+
+    private static void feignWithHystrixFallback() {
+        PersonServiceClient fallback = new PersonServiceClient() {
+            @Override
+            public Person get(@Param("id") long id) {
+                return new Person("fallback", 0);
+            }
+
+            @Override
+            public Person create(Person person) {
+                return person;
+            }
+        };
+
+        PersonServiceClient client = HystrixFeign.builder().
+                encoder(new JacksonEncoder()).
+                decoder(new JacksonDecoder()).
+                target(PersonServiceClient.class, "http://localhost:8100", fallback);
+
+        System.err.println(client.get(1));
+    }
+
+    private static void feign() {
+        PersonServiceClient client = Feign.builder().
+                encoder(new JacksonEncoder()).
+                decoder(new JacksonDecoder()).
+                target(PersonServiceClient.class, "http://localhost:8100");
+
+        System.err.print(client.get(1));
+    }
+
+    private static void basicHystrixCommand() {
         Person juntao = new Person("Juntao", 31);
         PersonCommand personCommand = new PersonCommand(juntao);
 
         Person result = personCommand.execute();
         System.err.println(result);
-
-        PersonServiceClient person = Feign.builder().encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(PersonServiceClient.class, "http://localhost:8100");
-
-        System.err.print(person.get(1));
     }
+
 }
--- src/main/java/com/thoughtworks/fallback/client/PersonServiceClient.java
+++ src/main/java/com/thoughtworks/fallback/client/PersonServiceClient.java
@@ -7,4 +7,7 @@ import feign.RequestLine;
 public interface PersonServiceClient {
     @RequestLine(value = "GET /people/{id}")
     Person get(@Param("id") long id);
+
+    @RequestLine(value = "POST /people")
+    Person create(Person person);
 }
